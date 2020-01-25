15763b3be79ffd1c649975312e9c545dbfad4b70
savezsiddiqui <savezsiddiqui@gmail.com>
2020-01-25 17:16:02 +0530

    antoher small change

--- src/reducers/DashboardReducer.js
+++ src/reducers/DashboardReducer.js
@@ -1,22 +1,21 @@
 import {
-    DASHBOARD_PAGE_DATA_FETCHED, DASHBOARD_TOGGLE_LOADING
-  } from '../action-types';
-  
-  
-  const INITIAL_STATE = {
-    dashboardData: {},
-    loading: true
-  };
-  
-  export default (state = INITIAL_STATE, action) => {
-      console.log('action', action.payload)
-      switch (action.type) {
-        case DASHBOARD_PAGE_DATA_FETCHED:
-            return { ...state, dashboardData: action.payload };
-        case DASHBOARD_TOGGLE_LOADING:
-            return {...state, loading: action.payload}
-        default:
-            return state;
-      }
-  };
-  
+  DASHBOARD_PAGE_DATA_FETCHED, DASHBOARD_TOGGLE_LOADING
+} from '../action-types';
+
+
+const INITIAL_STATE = {
+  dashboardData: {},
+  loading: true
+};
+
+export default (state = INITIAL_STATE, action) => {
+  // console.log('action', action)
+  switch (action.type) {
+    case DASHBOARD_PAGE_DATA_FETCHED:
+      return { ...state, dashboardData: action.payload };
+    case DASHBOARD_TOGGLE_LOADING:
+      return { ...state, loading: action.payload }
+    default:
+      return state;
+  }
+};
